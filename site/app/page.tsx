4341c3e0c397e6c4111771f05395221acd3d0d9a
Ranjan Srinivas <ranjan3101997@gmail.com>
2023-06-04 15:08:39 -0700

    commit

--- site/app/page.tsx
+++ site/app/page.tsx
@@ -1,32 +1,51 @@
 // import pgvector from 'pgvector/utils'
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from "@prisma/client";
+import { Configuration, OpenAIApi } from "openai";
 
 export default async function Home() {
+  const embedding = await getEmbedding("What is the RTX 4060ti review about?") as number[];
+  const prisma = new PrismaClient();
 
-  const embedding = getEmbedding("hello world");
-  const prisma = new PrismaClient()
-
-  const getNearestNeighbors = async (embedding: number[]) => {
-    const neighbors = await prisma.$queryRaw`
+  const getNearestNeighbors = async (
+    embedding: number[]
+  ): Promise<Neighbour[]> => {
+    const neighbors: Neighbour[] = await prisma.$queryRaw`
       SELECT id, embedding::text, meta FROM docs ORDER BY embedding <-> ${embedding}::vector LIMIT 5
-    `
+    `;
     return neighbors;
-  }
+  };
   const neighbors = await getNearestNeighbors(embedding);
-  console.log(neighbors[0].meta);
 
   return (
     <main>
       <h1>Hello world</h1>
     </main>
-  )
+  );
 }
 
+const getEmbedding = async (line: string) => {
+  try {
+    const config = new Configuration({
+      apiKey: process.env.OPENAI_KEY as string,
+    });
 
-const getEmbedding = (line: string)=> {
-  // get embedding from OpenAI
+    const openai = new OpenAIApi(config);
+    // ask chat gpt about query
+    const response = await openai.createEmbedding({
+      model: "text-embedding-ada-002",
+      input: line,
+    });
+    // get the answer back
+    const embedding = response.data.data[0].embedding;
 
-  // create a random array of 1536 numbers between -1 and 1  
-  const embedding = Array.from({length: 1536}, () => Math.random() * 2 - 1);
-  return embedding;
+    return embedding;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export interface Neighbour {
+  id: number;
+  embedding: number[];
+  meta: string;
 }
