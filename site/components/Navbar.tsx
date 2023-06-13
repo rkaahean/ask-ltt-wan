703d4ae50330347d66c697ccb809441ba3dc01bf
Ranjan Srinivas <ranjan3101997@gmail.com>
2023-06-12 21:31:32 -0700

    commit

--- site/components/Navbar.tsx
+++ site/components/Navbar.tsx
@@ -5,14 +5,14 @@ export const Navbar = () => {
     <div className="flex flex-row h-10 justify-center bg-black gap-5 my-3">
       <a
         className="bg-black hover:bg-black text-stone-800 hover:text-orange-500"
-        href="www.github.com/rkaahean"
+        href="https://www.github.com/rkaahean"
         target="_blank"
       >
         <GitHubLogoIcon className="h-8 w-8" />
       </a>
       <a
         className="bg-black hover:bg-black text-stone-800 hover:text-orange-500"
-        href="www.twitter.com/rkaahean"
+        href="https://www.twitter.com/rkaahean"
         target="_blank"
       >
         <TwitterLogoIcon className="h-8 w-8" />
