95ccd032e7e43b98e378c0671f2a27939a5cf422
ranjan <ranjan@data-sleek.com>
2023-06-13 13:20:13 -0700

    overflow

--- site/app/layout.tsx
+++ site/app/layout.tsx
@@ -1,26 +1,26 @@
-import { Navbar } from "@/components/Navbar";
-import "./globals.css";
-import { Inter } from "next/font/google";
-import { cn } from "@/lib/utils";
+import { Navbar } from '@/components/Navbar';
+import './globals.css';
+import { Inter } from 'next/font/google';
+import { cn } from '@/lib/utils';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-  title: "ask-ltt-wan",
-  description: "Ask anything about the LTT WAN show!",
+	title: 'ask-ltt-wan',
+	description: 'Ask anything about the LTT WAN show!',
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body className={cn(inter.className, "bg-black")}>
-        <Navbar />
-        {children}
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body className={cn(inter.className, 'bg-black', 'overflow-x-hidden')}>
+				<Navbar />
+				{children}
+			</body>
+		</html>
+	);
 }
--- site/components/Navbar.tsx
+++ site/components/Navbar.tsx
@@ -1,22 +1,22 @@
-import { GitHubLogoIcon, TwitterLogoIcon } from "@radix-ui/react-icons";
+import { GitHubLogoIcon, TwitterLogoIcon } from '@radix-ui/react-icons';
 
 export const Navbar = () => {
-  return (
-    <div className="flex flex-row h-10 justify-center bg-black gap-5 my-3">
-      <a
-        className="bg-black hover:bg-black text-stone-800 hover:text-orange-500"
-        href="https://www.github.com/rkaahean"
-        target="_blank"
-      >
-        <GitHubLogoIcon className="h-8 w-8" />
-      </a>
-      <a
-        className="bg-black hover:bg-black text-stone-800 hover:text-orange-500"
-        href="https://www.twitter.com/rkaahean"
-        target="_blank"
-      >
-        <TwitterLogoIcon className="h-8 w-8" />
-      </a>
-    </div>
-  );
+	return (
+		<div className="flex flex-row h-10 w-full justify-center bg-black gap-5 my-3">
+			<a
+				className="text-stone-700 hover:text-orange-500"
+				href="https://www.github.com/rkaahean"
+				target="_blank"
+			>
+				<GitHubLogoIcon className="h-8 w-8" />
+			</a>
+			<a
+				className="text-stone-700 hover:text-orange-500"
+				href="https://www.twitter.com/rkaahean"
+				target="_blank"
+			>
+				<TwitterLogoIcon className="h-8 w-8" />
+			</a>
+		</div>
+	);
 };
